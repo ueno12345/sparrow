d9b6a1ff31a2aba981c660bef47c353ee71a76fd
matsuda0528 <p99y92og@s.okayama-u.ac.jp>
2021-11-16 15:52:46 +0900

    Add ast class

---
+++ lib/swallow/ast.rb
@@ -0,0 +1,25 @@
+require 'sycamore/extension'
+
+module Swallow
+  class AST
+    #NOTE: ASTクラスはActiveRecordベースに作成
+    def initialize
+      @tree = Tree.new
+    end
+
+    def to_s
+      # return Tree Structure as String
+      @tree.to_s
+    end
+
+    def to_auk
+    end
+
+    def to_dimacs_cnf
+    end
+
+    def append( resource )
+      @tree << resource
+    end
+  end
+end
---
+++ lib/swallow/resource.rb
@@ -0,0 +1,93 @@
+class Resource
+  def initialize(name=nil)
+    @name = name
+    @domain = Domain.new
+    @belongs_to
+  end
+end
+
+class Period < Resource
+  def nr_days_a_week(num)
+    @domain.by_period.nr_days_a_week = num
+  end
+
+  def nr_periods(num)
+    @domain.by_period.nr_periods = num
+  end
+end
+
+class Room < Resource
+  def belongs_to(name)
+    @belongs_to = name
+  end
+
+  def unavaibale(start_time: nil, end_time: nil)
+    @domain.by_period.start_time = Time.parse start_time
+    @domain.by_period.end_time = Time.parse end_time
+  end
+end
+
+class Instructor < Resource
+  def belongs_to(name)
+    @belongs_to = name
+  end
+
+  def unavaibale(start_time: nil, end_time: nil)
+    @domain.by_period.start_time = Time.parse start_time
+    @domain.by_period.end_time = Time.parse end_time
+  end
+end
+
+class Lecture < Resource
+  def rooms(*rooms)
+    @domain.by_room.rooms = rooms
+  end
+
+  def instructors(*instructors)
+    @domain.by_instructor.instructors = instructors
+  end
+
+  def period(*period)
+    @domain.by_period.period = period
+  end
+
+  def term(term)
+    @domain.by_period.term = term
+  end
+
+  def belongs_to(name)
+    @belongs_to = name
+  end
+end
+
+class Domain
+  attr_reader :by_period
+  attr_reader :by_room
+  attr_reader :by_instructor
+  def initialize
+    @@nr_resources
+    @constraints = []
+    @by_period = DomainByPeriod.new
+    @by_room = DomainByRoom.new
+    @by_instructor = DomainByInstructor.new
+  end
+end
+
+class DomainByPeriod
+  attr_accessor :nr_days_a_week
+  attr_accessor :nr_periods
+
+  attr_accessor :unavailable
+  attr_accessor :term
+  attr_accessor :period
+  attr_accessor :start_time
+  attr_accessor :end_time
+end
+
+class DomainByRoom
+  attr_accessor :rooms
+end
+
+class DomainByInstructor
+  attr_accessor :instructors
+end
