c7e2f91f11a1a2bd2baf2e64a55aaadfe125d37c
matsuda0528 <p99y92og@s.okayama-u.ac.jp>
2021-12-09 16:36:28 +0900

    Rename to_dimacs method to to_cnf method

--- lib/swallow/ast.rb
+++ lib/swallow/ast.rb
@@ -1,5 +1,6 @@
 require "sycamore/extension"
 # TODO: RubyTree検討
+# NOTE: そもそも，Treeである必要があるか
 
 module Swallow
   class AST < Tree
@@ -11,10 +12,10 @@ module Swallow
       auk
     end
 
-    def to_dimacs(_prop_table)
-      dimacs_cnf = "" # TODO: 基本となる制約（CNF）を初期値として代入
+    def to_cnf(ptable)
+      cnf = Ravensat::PropLogic # TODO: 基本となる制約（CNF）を初期値として代入
       nodes.each do |node|
-        dimacs_cnf << node.to_dimacs_cnf
+        cnf &= node.to_cnf(ptable) # NOTE: Dependency Injection
       end
     end
   end
